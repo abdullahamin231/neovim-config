442e5994f0a48853d10f41ef6e8aa94261035bd3
Abdullah Amin <abdullahamin231@gmail.com>
2025-02-18 17:35:32 +0500

    Add more plugins

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -84,10 +84,35 @@ _G.packer_plugins = {
     path = "/home/abdullah/.local/share/nvim/site/pack/packer/start/bg.nvim",
     url = "https://github.com/typicode/bg.nvim"
   },
+  ["cmp-buffer"] = {
+    loaded = true,
+    path = "/home/abdullah/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    url = "https://github.com/hrsh7th/cmp-buffer"
+  },
   ["cmp-nvim-lsp"] = {
     loaded = true,
     path = "/home/abdullah/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
+  },
+  ["cmp-nvim-lsp-signature-help"] = {
+    loaded = true,
+    path = "/home/abdullah/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp-signature-help",
+    url = "https://github.com/hrsh7th/cmp-nvim-lsp-signature-help"
+  },
+  ["cmp-nvim-lua"] = {
+    loaded = true,
+    path = "/home/abdullah/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
+    url = "https://github.com/hrsh7th/cmp-nvim-lua"
+  },
+  ["cmp-path"] = {
+    loaded = true,
+    path = "/home/abdullah/.local/share/nvim/site/pack/packer/start/cmp-path",
+    url = "https://github.com/hrsh7th/cmp-path"
+  },
+  ["cmp-vsnip"] = {
+    loaded = true,
+    path = "/home/abdullah/.local/share/nvim/site/pack/packer/start/cmp-vsnip",
+    url = "https://github.com/hrsh7th/cmp-vsnip"
   },
   harpoon = {
     loaded = true,
@@ -171,6 +196,12 @@ _G.packer_plugins = {
     path = "/home/abdullah/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["todo-comments.nvim"] = {
+    config = { "\27LJ\2\n�\n\0\0\6\0001\00096\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\a\0005\4\4\0005\5\5\0=\5\6\4=\4\b\0035\4\t\0=\4\n\0035\4\v\0=\4\f\0035\4\r\0005\5\14\0=\5\6\4=\4\15\0035\4\16\0005\5\17\0=\5\6\4=\4\18\0035\4\19\0005\5\20\0=\5\6\4=\4\21\0035\4\22\0005\5\23\0=\5\6\4=\4\24\3=\3\25\0025\3\26\0=\3\27\0025\3\28\0004\4\0\0=\4\29\3=\3\30\0025\3 \0005\4\31\0=\4!\0035\4\"\0=\4#\0035\4$\0=\4%\0035\4&\0=\4'\0035\4(\0=\4)\0035\4*\0=\4+\3=\3,\0025\3-\0005\4.\0=\4/\3=\0030\2B\0\2\1K\0\1\0\vsearch\targs\1\6\0\0\18--color=never\17--no-heading\20--with-filename\18--line-number\r--column\1\0\3\fpattern\18\\b(KEYWORDS):\targs\0\fcommand\arg\vcolors\ttest\1\3\0\0\15Identifier\f#FF00FF\fdefault\1\3\0\0\15Identifier\f#7C3AED\thint\1\3\0\0\19DiagnosticHint\f#10B981\tinfo\1\3\0\0\19DiagnosticInfo\f#2563EB\fwarning\1\4\0\0\19DiagnosticWarn\15WarningMsg\f#FBBF24\nerror\1\0\6\tinfo\0\fdefault\0\thint\0\fwarning\0\nerror\0\ttest\0\1\4\0\0\20DiagnosticError\rErrorMsg\f#DC2626\14highlight\fexclude\1\0\n\fpattern\22.*<(KEYWORDS)\\s*:\vbefore\5\22multiline_context\3\n\22multiline_pattern\a^.\14multiline\2\fexclude\0\17max_line_len\3�\3\18comments_only\2\fkeyword\twide\nafter\afg\14gui_style\1\0\2\afg\tNONE\abg\tBOLD\rkeywords\tTEST\1\4\0\0\fTESTING\vPASSED\vFAILED\1\0\3\balt\0\ncolor\ttest\ticon\t⏲ \tNOTE\1\2\0\0\tINFO\1\0\3\balt\0\ncolor\thint\ticon\t \tPERF\1\4\0\0\nOPTIM\16PERFORMANCE\rOPTIMIZE\1\0\2\balt\0\ticon\t \tWARN\1\3\0\0\fWARNING\bXXX\1\0\3\balt\0\ncolor\fwarning\ticon\t \tHACK\1\0\2\ncolor\fwarning\ticon\t \tTODO\1\0\2\ncolor\tinfo\ticon\t \bFIX\1\0\a\bFIX\0\tTEST\0\tWARN\0\tPERF\0\tNOTE\0\tHACK\0\tTODO\0\balt\1\5\0\0\nFIXME\bBUG\nFIXIT\nISSUE\1\0\3\balt\0\ncolor\nerror\ticon\t \1\0\b\14gui_style\0\rkeywords\0\18sign_priority\3\b\nsigns\2\14highlight\0\vcolors\0\19merge_keywords\2\vsearch\0\nsetup\18todo-comments\frequire\0" },
+    loaded = true,
+    path = "/home/abdullah/.local/share/nvim/site/pack/packer/start/todo-comments.nvim",
+    url = "https://github.com/folke/todo-comments.nvim"
+  },
   undotree = {
     loaded = true,
     path = "/home/abdullah/.local/share/nvim/site/pack/packer/start/undotree",
@@ -180,10 +211,19 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/abdullah/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-vsnip"] = {
+    loaded = true,
+    path = "/home/abdullah/.local/share/nvim/site/pack/packer/start/vim-vsnip",
+    url = "https://github.com/hrsh7th/vim-vsnip"
   }
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: todo-comments.nvim
+time([[Config for todo-comments.nvim]], true)
+try_loadstring("\27LJ\2\n�\n\0\0\6\0001\00096\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\a\0005\4\4\0005\5\5\0=\5\6\4=\4\b\0035\4\t\0=\4\n\0035\4\v\0=\4\f\0035\4\r\0005\5\14\0=\5\6\4=\4\15\0035\4\16\0005\5\17\0=\5\6\4=\4\18\0035\4\19\0005\5\20\0=\5\6\4=\4\21\0035\4\22\0005\5\23\0=\5\6\4=\4\24\3=\3\25\0025\3\26\0=\3\27\0025\3\28\0004\4\0\0=\4\29\3=\3\30\0025\3 \0005\4\31\0=\4!\0035\4\"\0=\4#\0035\4$\0=\4%\0035\4&\0=\4'\0035\4(\0=\4)\0035\4*\0=\4+\3=\3,\0025\3-\0005\4.\0=\4/\3=\0030\2B\0\2\1K\0\1\0\vsearch\targs\1\6\0\0\18--color=never\17--no-heading\20--with-filename\18--line-number\r--column\1\0\3\fpattern\18\\b(KEYWORDS):\targs\0\fcommand\arg\vcolors\ttest\1\3\0\0\15Identifier\f#FF00FF\fdefault\1\3\0\0\15Identifier\f#7C3AED\thint\1\3\0\0\19DiagnosticHint\f#10B981\tinfo\1\3\0\0\19DiagnosticInfo\f#2563EB\fwarning\1\4\0\0\19DiagnosticWarn\15WarningMsg\f#FBBF24\nerror\1\0\6\tinfo\0\fdefault\0\thint\0\fwarning\0\nerror\0\ttest\0\1\4\0\0\20DiagnosticError\rErrorMsg\f#DC2626\14highlight\fexclude\1\0\n\fpattern\22.*<(KEYWORDS)\\s*:\vbefore\5\22multiline_context\3\n\22multiline_pattern\a^.\14multiline\2\fexclude\0\17max_line_len\3�\3\18comments_only\2\fkeyword\twide\nafter\afg\14gui_style\1\0\2\afg\tNONE\abg\tBOLD\rkeywords\tTEST\1\4\0\0\fTESTING\vPASSED\vFAILED\1\0\3\balt\0\ncolor\ttest\ticon\t⏲ \tNOTE\1\2\0\0\tINFO\1\0\3\balt\0\ncolor\thint\ticon\t \tPERF\1\4\0\0\nOPTIM\16PERFORMANCE\rOPTIMIZE\1\0\2\balt\0\ticon\t \tWARN\1\3\0\0\fWARNING\bXXX\1\0\3\balt\0\ncolor\fwarning\ticon\t \tHACK\1\0\2\ncolor\fwarning\ticon\t \tTODO\1\0\2\ncolor\tinfo\ticon\t \bFIX\1\0\a\bFIX\0\tTEST\0\tWARN\0\tPERF\0\tNOTE\0\tHACK\0\tTODO\0\balt\1\5\0\0\nFIXME\bBUG\nFIXIT\nISSUE\1\0\3\balt\0\ncolor\nerror\ticon\t \1\0\b\14gui_style\0\rkeywords\0\18sign_priority\3\b\nsigns\2\14highlight\0\vcolors\0\19merge_keywords\2\vsearch\0\nsetup\18todo-comments\frequire\0", "config", "todo-comments.nvim")
+time([[Config for todo-comments.nvim]], false)
 -- Config for: kanagawa
 time([[Config for kanagawa]], true)
 try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\25colorscheme kanagawa\bcmd\bvim\0", "config", "kanagawa")
